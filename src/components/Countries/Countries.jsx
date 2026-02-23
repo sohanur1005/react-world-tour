919ee5338c053dd8a2fa76aeccdafe241d270d07
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-24 01:47:35 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+
+
+const Countries = () => {
+ 
+  const [countries,setCountries]=useState([]);
+  useEffect(()=>{
+      fetch("https://restcountries.com/v3.1/all?fields=name,flags,cca3")
+      .then(res=> res.json())
+      .then(data=>{
+        console.log(data);
+        setCountries(data)
+      } );
+
+  },[])
+
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country></Country>)
+      }
+    </div>
+  );
+};
+
+export default Countries;
